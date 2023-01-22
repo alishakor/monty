645c3fda50525fb85fb7f461044255476633a0ca
alishakor <aliuokeowo@gmail.com>
2023-01-23 00:07:41 +0100

    main function

--- main.c
+++ main.c
@@ -1,29 +1,30 @@
 #include "monty.h"
 
 
-variables var = {NULL, NULL};
+variables var = {NULL, NULL, STACK};
 
 /**
  * main - A monty scripting language
  * @ac: Argument counter
  * @av: Argument array
  *
- * Return: 0 on success
+ * Return: exit_status
  */
-int main(int ac, char*av[])
+int main(int ac, char *av[])
 {
 	FILE *fp;
+
 	if (ac != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
-
+		exit(EXIT_FAILURE);
+	}
 
 	fp = fopen(av[1], "r");
-	if (fp = NULL)
+	if (fp == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n",av[0]);
-		return (EXIT_FAILURE);
+		fprintf(stderr, "Error: Can't open file %s\n", av[0]);
+		exit(EXIT_FAILURE);
 	}
 
 	execute_monty(fp);
