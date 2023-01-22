214973c428931f7b7ba3dc2b911b7615662c4b6f
Sideeqkolade <sideeqkolade@gmail.com>
2023-01-22 23:59:55 +0000

    utilities fixed

--- utilities.c
+++ utilities.c
@@ -21,3 +21,58 @@ int check_arg(char *arg)
 	}
 	return (1);
 }
+
+/**
+ * add_node_stack - A fuction that add node stack
+ * @head: head counter
+ * @n: Integer argument
+ */
+void add_node_stack(stack_t **head, int n)
+{
+	stack_t *tmp, *new;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+		malloc_error(head);
+
+	if (*head)
+	{
+		tmp = *head;
+		new->n = n;
+		new->next = tmp;
+		new->prev = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->n = n;
+		new->prev = NULL;
+		new->next = NULL;
+		*head = new;
+	}
+}
+
+/**
+ * add_node_queue - A function to add node to queue
+ * @head: Head counter
+ * @n: Integer Argument
+ */
+void add_node_queue(stack_t **head, int n)
+{
+	stack_t *tmp, *new;
+
+	new = malloc(sizeof(stack_t));
+
+	if (new == NULL)
+		malloc_error(head);
+
+	if (*head == NULL || head == NULL)
+	{
+		tmp = *head;
+		while (tmp->next)
+			tmp = tmp->next;
+		new->next = NULL;
+		new->prev = tmp;
+		tmp->next = new;
+	}
+}
