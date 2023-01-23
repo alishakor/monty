c1491ed8429b83d9687258405003e2b1a617c9d9
Sideeqkolade <sideeqkolade@gmail.com>
2023-01-23 00:59:47 +0000

    op_code_func_4 added

---
+++ op_code_func_4.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * monty_mode - A function for monty mode
+ * @stack: Stack Argument
+ * @line_number: Number Argument
+ */
+void monty_mode(stack_t **stack, unsigned int line_number)
+{
+	if (strcmp(var.opcode, "queue") == 0)
+		var.mode = QUEUE;
+	if (strcmp(var.opcode, "stack") == 0)
+		var.mode = STACK;
+	(void)stack;
+	(void)line_number;
+}
