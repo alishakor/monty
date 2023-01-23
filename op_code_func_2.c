bb357f20de07b86fbd58bad42cf1ad4c94b1fefe
Sideeqkolade <sideeqkolade@gmail.com>
2023-01-23 00:54:09 +0000

    op_code_func_3 added

--- op_code_func_2.c
+++ op_code_func_2.c
@@ -20,16 +20,92 @@ void monty_pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *node1, *node2;
 
-	if ((*stack)->next == NULL)
+	if ((*stack) == NULL)
 	{
-		pop_error(line_number);
+		pop_error(stack, line_number);
 	}
 
-	pop_error(line_number);
+	node1 = (*stack);
 	node2 = node1->next;
 
-	(*stack)->next = node2;
-	node2->prev = *stack;
+	if (node2 == NULL)
+	{
+		*stack = NULL;
+		free(node1);
+	}
+	else
+	{
+		(*stack) = node2;
+		node2->prev = *stack;
+		free(node1);
+	}
+}
 
+/**
+ * monty_sub- A function that prints monty add
+ * @stack: Stack Argument
+ * @line_number: Number Argument
+ */
+void monty_sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node1, *node2;
+
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		sub_error(stack, line_number);
+	}
+
+	node1 = (*stack);
+	node2 = node1->next;
+	node2->n -= node1->n;
+	(*stack) = node2;
+	free(node1);
+}
+
+/**
+ * monty_div - A function monty division
+ * @stack: Stack Argument
+ * @line_number: Number Argument
+ */
+void monty_div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node1, *node2;
+
+	node1 = (*stack);
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		div_error(stack, line_number);
+	}
+	if (node1->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free_list(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	node2 = node1->next;
+	node2->n /= node1->n;
+	(*stack) = node2;
+	free(node1);
+}
+
+/**
+ * monty_mul - A function for stack monty multiplication
+ * @stack: Stack Argument
+ * @line_number: Number Argument
+ */
+void monty_mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node1, *node2;
+
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		mul_error(stack, line_number);
+	}
+
+	node1 = (*stack);
+	node2 = node1->next;
+	node2->n *= node1->n;
+	(*stack) = node2;
 	free(node1);
 }
---
+++ op_code_func_3.c
@@ -0,0 +1,132 @@
+#include "monty.h"
+
+/**
+ * monty_mod - A function that prints monty  mod
+ * @stack: List Argument
+ * @line_number: Number Argument
+ */
+void monty_mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node1, *node2;
+
+	node1 = (*stack);
+	if (node1->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free_list(stack);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack) == NULL || (*stack)->next == NULL)
+	{
+		mod_error(stack, line_number);
+	}
+
+	node2 = node1->next;
+	node2->n %= node1->n;
+	(*stack) = node2;
+	free(node1);
+}
+
+/**
+ * monty_pchar - A function that prints character in monty
+ * @stack: List Argument
+ * @line_number: Number Argument
+ *
+ * Return: EXIT_FAILURE
+ */
+void monty_pchar(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node1;
+
+	if ((*stack) == NULL)
+	{
+		pchar_error(stack, line_number);
+	}
+	node1 = (*stack);
+	if (node1->n > 127 || node1->n < 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		free_list(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%c\n", node1->n);
+}
+
+/**
+ * monty_pstr - A function for monty pstr
+ * @stack: List Argument
+ * @line_number: Number Argument
+ *
+ * Return: EXIT_FAILURE
+ */
+void monty_pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	temp = *stack;
+
+	while (temp)
+	{
+		if (!(temp->n > 127 || temp->n < 0))
+		{
+			printf("%c", temp->n);
+			temp = temp->next;
+		}
+		else
+			break;
+	}
+	printf("\n");
+	(void)line_number;
+}
+
+/**
+ * monty_rotl - A function for stack rotl
+ * @stack: List Argument
+ * @line_number: Number Argument
+ */
+void monty_rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp, *top;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
+
+	top = (*stack)->next;
+	top->prev = NULL;
+
+	temp = *stack;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = *stack;
+	(*stack)->next = NULL;
+	(*stack)->prev = temp;
+	(*stack) = temp;
+	(void)line_number;
+}
+
+/**
+ * monty_rotr - A function for stack rotation_right
+ * @stack: List Argument
+ * @line_number: Number Argument
+ *
+ */
+void monty_rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
+
+	temp = *stack;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = *stack;
+	temp->prev->next = NULL;
+	temp->prev = NULL;
+	(*stack)->prev = temp;
+	(*stack) = temp;
+	(void)line_number;
+}
